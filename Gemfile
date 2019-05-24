5c191058971463b86fbda5ca4c1d5280b05f2d0c
Phat Phan <s3672747@student.rmit.edu.au>
2019-05-24 04:05:02 +0000

    week 10

--- Gemfile
+++ Gemfile
@@ -10,6 +10,9 @@ gem 'jquery-rails', '4.3.1'
 gem 'turbolinks',   '5.0.1'
 gem "bootstrap-sass", ">= 3.4.1"
 gem 'jbuilder',     '2.7.0'
+gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 
 group :production do
   gem 'pg', '0.20.0'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,18 +5,19 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
-
-  # Add more helper methods to be used by all tests here...
-  
   # Returns true if a test user is logged in.
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
 end
-  
+
 class ActionDispatch::IntegrationTest
 
   # Log in as a particular user.
